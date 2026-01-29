3ead015423060d5fb242bb24e6b743ee1dccc516
liogedz <aleksei.gedz@gmail.com>
2026-01-29 18:10:09 +0200

    chore: normalize line endings

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,26 +1,15 @@
-import {Component, effect, HostBinding, signal} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
+import {DarkThemeSelectorService} from './services/dark-theme-selector-service';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
-  protected readonly title = signal('frontend');
-
-  darkMode = signal<boolean>(
-    JSON.parse(window.localStorage.getItem("darkMode") ?? "false")
-  );
-
-  @HostBinding("class.dark") get mode() {
-    return this.darkMode();
+  constructor(protected darkThemeSelectorService: DarkThemeSelectorService) {
   }
 
-  constructor() {
-    effect(() => {
-      window.localStorage.setItem("darkMode", JSON.stringify(this.darkMode()));
-    });
-  }
 }
---
+++ frontend/src/app/services/dark-theme-selector-service.ts
@@ -0,0 +1,89 @@
+import {Injectable, signal} from '@angular/core';
+
+export enum AppTheme {
+  LIGHT = 'light',
+  DARK = 'dark',
+}
+
+const CLIENT_RENDER = typeof localStorage !== 'undefined';
+const LS_THEME = 'theme';
+let selectedTheme: AppTheme | undefined = undefined;
+
+if (CLIENT_RENDER) {
+  selectedTheme = localStorage.getItem(LS_THEME) as AppTheme || undefined;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DarkThemeSelectorService {
+  currentTheme = signal<AppTheme | undefined>(selectedTheme);
+
+  toggleTheme() {
+    const current = this.currentTheme();
+
+    if (current === AppTheme.LIGHT) {
+      this.setDarkTheme();
+    } else if (current === AppTheme.DARK) {
+      this.setSystemTheme();
+    } else {
+      this.setLightTheme();
+    }
+  }
+
+
+  setLightTheme() {
+    this.currentTheme.set(AppTheme.LIGHT);
+    this.setToLocalStorage(AppTheme.LIGHT);
+    this.removeClassFromHtml('dark');
+  }
+
+  setDarkTheme() {
+    this.currentTheme.set(AppTheme.DARK);
+    this.setToLocalStorage(AppTheme.DARK);
+    this.addClassToHtml('dark');
+  }
+
+  setSystemTheme() {
+    this.currentTheme.set(undefined);
+    this.removeFromLocalStorage();
+    if (isSystemDark()) {
+      this.addClassToHtml('dark');
+    } else {
+      this.removeClassFromHtml('dark');
+    }
+  }
+
+  private addClassToHtml(className: string) {
+    if (CLIENT_RENDER) {
+      this.removeClassFromHtml(className);
+      document.documentElement.classList.add(className)
+    }
+  }
+
+  private removeClassFromHtml(className: string) {
+    if (CLIENT_RENDER) {
+      document.documentElement.classList.remove(className)
+    }
+  }
+
+  private setToLocalStorage(theme: AppTheme) {
+    if (CLIENT_RENDER) {
+      localStorage.setItem(LS_THEME, theme);
+    }
+  }
+
+  private removeFromLocalStorage() {
+    if (CLIENT_RENDER) {
+      localStorage.removeItem(LS_THEME);
+    }
+  }
+}
+
+function isSystemDark() {
+  if (typeof window !== 'undefined') {
+    return window.matchMedia('(prefers-color-scheme: dark)').matches;
+  } else {
+    return false;
+  }
+}
